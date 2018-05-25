40b3963b4d7127840dd475b307ba7bd66941924c
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-05-25 11:13:44 -0300

    Add professores migration

---
+++ database/migrations/2018_05_19_193212_create_professores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfessoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('professor_siape')->unique();
+            $table->string('professor_nome');
+            $table->integer('departamento_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('departamento_id')
+                ->references('id')
+                ->on('departamentos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professores');
+    }
+}
