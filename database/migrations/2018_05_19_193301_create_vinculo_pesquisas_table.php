a0b60d0a8bd50bc939907f4de393c0b185923d52
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-05-25 11:14:55 -0300

    Add vinculo_pesquisas migration

---
+++ database/migrations/2018_05_19_193301_create_vinculo_pesquisas_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVinculoPesquisasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vinculo_pesquisas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pesquisa_id')->unsigned();
+            $table->integer('professor_papel_id')->unsigned();
+            $table->integer('aluno_id')->unsigned();
+            $table->integer('professor_id')->unsigned();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+            $table->foreign('pesquisa_id')
+                ->references('id')
+                ->on('pesquisas');
+
+            $table->foreign('professor_papel_id')
+                ->references('id')
+                ->on('professor_papeis');
+
+            $table->foreign('aluno_id')
+                ->references('id')
+                ->on('alunos');
+
+            $table->foreign('professor_id')
+                ->references('id')
+                ->on('professores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vinculo_pesquisas');
+    }
+}
