ad72159bbc015fff1268faa7eeb8b7fb77b9c9ea
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-06-01 00:23:29 -0300

    Add methods to check user roles and add relationship from model user to model role

--- app/User.php
+++ app/User.php
@@ -26,4 +26,39 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles(){
+        return $this->belongsToMany('PesquisaProjeto\Role');
+    }
+
+    public function hasAnyRole($roles)
+    {
+
+        if( !is_array($roles) && !$this->hasRole($roles) ){
+            return false;
+        }
+
+        if( is_array($roles) ){
+            foreach($roles as $role){
+                if( $this->hasRole($role) ){
+                    return true;
+                }
+            }
+        }else{
+            if($this->hasRole($roles)){
+                    return true;
+                }
+        }
+
+        return false;
+    }
+
+    private function hasRole($role)
+    {
+        if($this->roles()->where('name',$role)->first()){
+            return true;
+        }
+
+        return false;
+    }
 }
