7db15ee9027c52de343b30bc5232cd8dbaac2044
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-06-01 00:25:32 -0300

    Create default users for ACL tests

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use PesquisaProjeto\User;
+use PesquisaProjeto\Role;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(PesquisaProjeto\User::class)->create();
+        // factory(PesquisaProjeto\User::class)->create();
+        $role_admin = Role::where('name','admin')->first();
+        $role_user = Role::where('name','user')->first();
+
+        $admin = new User();
+        $admin->name = "admin";
+        $admin->email = "admin@admin.com";
+        $admin->password = password_hash('admin',PASSWORD_DEFAULT);
+        $admin->save();
+        $admin->roles()->attach($role_admin);
+
+        $user = new User();
+        $user->name = "ufop";
+        $user->email = "ufop@ufop.com";
+        $user->password = password_hash('ufop',PASSWORD_DEFAULT);
+        $user->save();
+        $user->roles()->attach($role_user);
     }
 }
