757a13440c52905f6943fc0bf6ca46712083cf1f
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-06-11 10:48:14 -0300

    Add constants to intentify professor_role on ProfessorPapel model

--- app/ProfessorPapel.php
+++ app/ProfessorPapel.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProfessorPapel extends Model
 {
     protected $table = 'professor_papeis';
+
+    const ORIENTADOR = 1;
+    const COORIENTADOR = 2;
 }
