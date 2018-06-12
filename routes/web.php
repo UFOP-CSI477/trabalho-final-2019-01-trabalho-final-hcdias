44de22e30be93956afbd45f7e9d4a8c754b93986
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-06-11 21:47:42 -0300

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,38 @@ Route::get('/visualizar-usuario', [
 	'roles'=>'admin',
 	'middleware'=>'roles'
 	]);
+
+Route::get('/criar-usuario', [
+	'as'=>'criar_usuario',
+	'uses'=>'UserController@create',
+	'roles'=>'admin',
+	'middleware'=>'roles'
+	]);
+
+Route::post('/salvar-usuario', [
+	'as'=>'salvar_usuario',
+	'uses'=>'UserController@store',
+	'roles'=>'admin',
+	'middleware'=>'roles'
+	]);
+
+Route::get('/editar-usuario/{id}', [
+	'as'=>'editar_usuario',
+	'uses'=>'UserController@edit',
+	'roles'=>'admin',
+	'middleware'=>'roles'
+	]);
+
+Route::post('/alterar-usuario/{id}', [
+	'as'=>'alterar_usuario',
+	'uses'=>'UserController@update',
+	'roles'=>'admin',
+	'middleware'=>'roles'
+	]);
+
+Route::get('/remover-usuario/{id}', [
+	'as'=>'remover_usuario',
+	'uses'=>'UserController@destroy',
+	'roles'=>'admin',
+	'middleware'=>'roles'
+	]);
