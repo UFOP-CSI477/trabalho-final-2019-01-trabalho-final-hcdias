535a5d5a6b84686508c84c2ad4706a7d41c6ea51
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-06-13 18:18:27 -0300

    Change name generator for AlunoFactory

--- database/factories/AlunoFactory.php
+++ database/factories/AlunoFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
 $factory->define(PesquisaProjeto\Aluno::class, function (Faker $faker) {
     return [
     	'aluno_matricula'=>$faker->unique()->randomNumber(5),
-        'aluno_nome' => str_random(10),
+        'aluno_nome' => $faker->name,
         'curso_id'=>rand(1,5)
     ];
 });
