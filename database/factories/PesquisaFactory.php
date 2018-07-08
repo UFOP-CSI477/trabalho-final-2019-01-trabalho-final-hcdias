7c36bdcd18fe3540c8fcd9aef9ebfb5d56874858
marcobonelli <marco.bonelli.jr@gmail.com>
2018-07-08 18:39:56 -0300

    Update PesquisaFactory

--- database/factories/PesquisaFactory.php
+++ database/factories/PesquisaFactory.php
@@ -9,7 +9,13 @@ $factory->define(PesquisaProjeto\Pesquisa::class, function (Faker $faker) {
     	'pesquisa_resumo'=>$faker->text(200),
     	'pesquisa_ano_inicio'=>date('Y'),
     	'pesquisa_semestre_inicio'=>rand(1,2),
-    	'pesquisa_status'=>rand(1,6)
-
+    	'pesquisa_status'=>rand(1,6),
+    	'natureza_pesquisa_id'=>rand(1,2),
+    	'abordagem_pesquisa_id'=>rand(1,3),
+    	'objetivo_pesquisa_id'=>rand(1,4),
+    	'procedimentos_pesquisa_id'=>rand(1,6),
+    	'area_pesquisa_id'=>rand(1,11),
+    	'agencia_pesquisa_id'=>rand(1,3),
+    	'sub_area_pesquisa_id'=>rand(1,58)
     ];
 });
