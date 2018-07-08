e827819a8c3b38a65ccbcfe74473b81b2cccb0e8
marcobonelli <marco.bonelli.jr@gmail.com>
2018-07-08 18:36:27 -0300

    Add SubAreaPesquisaSeeder

---
+++ database/seeds/SubAreaPesquisaSeeder.php
@@ -0,0 +1,247 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use PesquisaProjeto\SubAreaPesquisa;
+
+class SubAreaPesquisaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $np1 = new SubAreaPesquisa();
+        $np1->descricao = 'Gestão de sistemas de produção';
+        $np1->save();
+
+        $np2 = new SubAreaPesquisa();
+        $np2->descricao = 'Planejamento e controle da produção';
+        $np2->save();
+
+        $np3 = new SubAreaPesquisa();
+        $np3->descricao = 'Logística e gestão da cadeia de suprimentos e distribuição';
+        $np3->save();
+
+        $np4 = new SubAreaPesquisa();
+        $np4->descricao = 'Projeto de fábrica e de instalações industriais';
+        $np4->save();
+
+        $np5 = new SubAreaPesquisa();
+        $np5->descricao = 'Gestão da manutenção';
+        $np5->save();
+
+        $np6 = new SubAreaPesquisa();
+        $np6->descricao = 'Simulação da produção';
+        $np6->save();
+
+        $np7 = new SubAreaPesquisa();
+        $np7->descricao = 'Gestão de processos produtivos';
+        $np7->save();
+
+        $np8 = new SubAreaPesquisa();
+        $np8->descricao = 'Gestão de operações e serviços';
+        $np8->save();
+
+        $np9 = new SubAreaPesquisa();
+        $np9->descricao = 'Controle estatístico da qualidade';
+        $np9->save();
+
+        $np10 = new SubAreaPesquisa();
+        $np10->descricao = 'Normalização e certificação para a qualidade';
+        $np10->save();
+
+        $np11 = new SubAreaPesquisa();
+        $np11->descricao = 'Organização metrológica da qualidade';
+        $np11->save();
+
+        $np12 = new SubAreaPesquisa();
+        $np12->descricao = 'Confiabilidade de processos e produtos';
+        $np12->save();
+
+        $np13 = new SubAreaPesquisa();
+        $np13->descricao = 'Qualidade em serviços';
+        $np13->save();
+
+        $np14 = new SubAreaPesquisa();
+        $np14->descricao = 'Engenharia econômica';
+        $np14->save();
+
+        $np15 = new SubAreaPesquisa();
+        $np15->descricao = 'Gestão de custos';
+        $np15->save();
+
+        $np16 = new SubAreaPesquisa();
+        $np16->descricao = 'Gestão financeira de projetos';
+        $np16->save();
+
+        $np17 = new SubAreaPesquisa();
+        $np17->descricao = 'Gestão de investimentos';
+        $np17->save();
+
+        $np18 = new SubAreaPesquisa();
+        $np18->descricao = 'Gestão de desempenho de sistemas de produção e operações';
+        $np18->save();
+
+        $np19 = new SubAreaPesquisa();
+        $np19->descricao = 'Projeto e organização do trabalho';
+        $np19->save();
+
+        $np20 = new SubAreaPesquisa();
+        $np20->descricao = 'Psicologia do trabalho';
+        $np20->save();
+
+        $np21 = new SubAreaPesquisa();
+        $np21->descricao = 'Biomecânica ocupacional';
+        $np21->save();
+
+        $np22 = new SubAreaPesquisa();
+        $np22->descricao = 'Projeto e gestão da segurança do trabalho';
+        $np22->save();
+
+        $np23 = new SubAreaPesquisa();
+        $np23->descricao = 'Análise e prevenção de riscos de acidentes';
+        $np23->save();
+
+        $np24 = new SubAreaPesquisa();
+        $np24->descricao = 'Ergonomia do produto';
+        $np24->save();
+
+        $np25 = new SubAreaPesquisa();
+        $np25->descricao = 'Ergonomia dos processos de produção';
+        $np25->save();
+
+        $np26 = new SubAreaPesquisa();
+        $np26->descricao = 'Pesquisa de mercado';
+        $np26->save();
+
+        $np27 = new SubAreaPesquisa();
+        $np27->descricao = 'Planejamento do produto';
+        $np27->save();
+
+        $np28 = new SubAreaPesquisa();
+        $np28->descricao = 'Metodologia de projeto do produto';
+        $np28->save();
+
+        $np29 = new SubAreaPesquisa();
+        $np29->descricao = 'Engenharia de produto';
+        $np29->save();
+
+        $np30 = new SubAreaPesquisa();
+        $np30->descricao = 'Marketing do produto';
+        $np30->save();
+
+        $np31 = new SubAreaPesquisa();
+        $np31->descricao = 'Programação matemática';
+        $np31->save();
+
+        $np32 = new SubAreaPesquisa();
+        $np32->descricao = 'Decisão multicriterial';
+        $np32->save();
+
+        $np33 = new SubAreaPesquisa();
+        $np33->descricao = 'Processos estocásticos';
+        $np33->save();
+
+        $np34 = new SubAreaPesquisa();
+        $np34->descricao = 'Modelagem, análise e simulação';
+        $np34->save();
+
+        $np35 = new SubAreaPesquisa();
+        $np35->descricao = 'Teoria da decisão e teoria dos jogos';
+        $np35->save();
+
+        $np36 = new SubAreaPesquisa();
+        $np36->descricao = 'Análise de demandas por produtos';
+        $np36->save();
+
+        $np37 = new SubAreaPesquisa();
+        $np37->descricao = 'Planejamento estratégico e operacional da estrutura organizacional';
+        $np37->save();
+
+        $np38 = new SubAreaPesquisa();
+        $np38->descricao = 'Estratégias de produção';
+        $np38->save();
+
+        $np39 = new SubAreaPesquisa();
+        $np39->descricao = 'Organização industrial';
+        $np39->save();
+
+        $np40 = new SubAreaPesquisa();
+        $np40->descricao = 'Gestão e estratégia de mercados e produtos';
+        $np40->save();
+
+        $np41 = new SubAreaPesquisa();
+        $np41->descricao = 'Redes de empresas e gestão da cadeia produtiva';
+        $np41->save();
+
+        $np42 = new SubAreaPesquisa();
+        $np42->descricao = 'Gestão da inovação';
+        $np42->save();
+
+        $np43 = new SubAreaPesquisa();
+        $np43->descricao = 'Gestão da tecnologia';
+        $np43->save();
+
+        $np44 = new SubAreaPesquisa();
+        $np44->descricao = 'Gestão da informação de produção e operações';
+        $np44->save();
+
+        $np45 = new SubAreaPesquisa();
+        $np45->descricao = 'Gestão de projetos';
+        $np45->save();
+
+        $np46 = new SubAreaPesquisa();
+        $np46->descricao = 'Gestão do conhecimento em sistemas produtivos';
+        $np46->save();
+
+        $np47 = new SubAreaPesquisa();
+        $np47->descricao = 'Gestão de recursos naturais';
+        $np47->save();
+
+        $np48 = new SubAreaPesquisa();
+        $np48->descricao = 'Gestão energética';
+        $np48->save();
+
+        $np49 = new SubAreaPesquisa();
+        $np49->descricao = 'Produção mais limpa e ecoeficiência';
+        $np49->save();
+
+        $np50 = new SubAreaPesquisa();
+        $np50->descricao = 'Gestão de resíduos industriais e prevenção de poluição';
+        $np50->save();
+
+        $np51 = new SubAreaPesquisa();
+        $np51->descricao = 'Estudo do ensino de engenharia de produção';
+        $np51->save();
+
+        $np52 = new SubAreaPesquisa();
+        $np52->descricao = 'Estudo do desenvolvimento e aplicação da pesquisa em engenharia de produção';
+        $np52->save();
+
+        $np53 = new SubAreaPesquisa();
+        $np53->descricao = 'Estudo da prática profissional em engenharia de produção';
+        $np53->save();
+
+        $np54 = new SubAreaPesquisa();
+        $np54->descricao = 'Ética e transparência nas decisões organizacionais';
+        $np54->save();
+
+        $np55 = new SubAreaPesquisa();
+        $np55->descricao = 'Governança organizacional';
+        $np55->save();
+
+        $np56 = new SubAreaPesquisa();
+        $np56->descricao = 'Responsabilidade social organizacional';
+        $np56->save();
+
+        $np57 = new SubAreaPesquisa();
+        $np57->descricao = 'Sustentabilidade e sistemas de indicadores';
+        $np57->save();
+
+        $np58 = new SubAreaPesquisa();
+        $np58->descricao = 'Desenvolvimento sustentável em engenharia de produção';
+        $np58->save();
+    }
+}
