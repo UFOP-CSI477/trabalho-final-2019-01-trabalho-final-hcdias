7521ec518f44c03ba486372d3419ed417589fc15
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-10-23 23:35:58 -0200

    Alterar atributo aluno_matricula para apenas matricula

--- database/factories/AlunoFactory.php
+++ database/factories/AlunoFactory.php
@@ -5,8 +5,8 @@ use Faker\Generator as Faker;
 
 $factory->define(PesquisaProjeto\Aluno::class, function (Faker $faker) {
     return [
-    	'aluno_matricula'=>$faker->unique()->randomNumber(5),
-        'aluno_nome' => $faker->name,
+    	'matricula'=>$faker->unique()->randomNumber(5),
+        'nome' => $faker->name,
         'curso_id'=>rand(1,5)
     ];
 });
