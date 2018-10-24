b64fa711f0e1ecb6333a6414e7c107141489c41b
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-10-23 23:50:32 -0200

    Adicionar paginas de status 403 e 404

---
+++ resources/views/404.blade.php
@@ -0,0 +1,38 @@
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1>Bem vindo, {{Auth::user()->name}}</h1>
+    <small>Acopanhe sua movimentação recente de projetos</small>
+@stop
+
+
+@section('content')
+	<section class="content">
+      <div class="error-page">
+        <h2 class="headline text-yellow"> 404</h2>
+
+        <div class="error-content">
+          <h3><i class="fa fa-warning text-yellow"></i> Oops! Página não encontrada.</h3>
+
+          <p>
+            Não foi possível encontrar o que você está procurando.
+            Volte ao dashboard para visualizar seus projetos.
+          </p>
+
+          <!-- <form class="search-form">
+            <div class="input-group">
+              <input type="text" name="search" class="form-control" placeholder="Search">
+
+              <div class="input-group-btn">
+                <button type="submit" name="submit" class="btn btn-warning btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </div>
+            </div>
+            <!-- /.input-group -->
+          <!-- </form>  -->
+        </div>
+        <!-- /.error-content -->
+      </div>
+      <!-- /.error-page -->
+    </section>
+    @stop
