2f93c5b1882c6cc13ccb97592e4ba5c0de88097b
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-10-23 23:36:34 -0200

    Alterar atributo professor_siape para apenas siape

--- database/factories/ProfessorFactory.php
+++ database/factories/ProfessorFactory.php
@@ -5,8 +5,8 @@ use Faker\Generator as Faker;
 
 $factory->define(PesquisaProjeto\Professor::class, function (Faker $faker) {
     return [
-    	'professor_siape'=>$faker->unique()->randomNumber(5),
-    	'professor_nome'=>$faker->name,
+    	'siape'=>$faker->unique()->randomNumber(5),
+    	'nome'=>$faker->name,
         'departamento_id'=>rand(1,5)
     ];
 });
