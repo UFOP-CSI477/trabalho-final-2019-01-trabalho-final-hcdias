1f939d767aa29878943ab34d50d306b89ac382ca
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-10-26 00:56:18 -0300

    Adicionar rotas para TCC

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,50 @@ Route::group(['prefix'=>'pesquisa'],function(){
 		]);
 });
 
+Route::group(['prefix'=>'tcc'],function(){
+	Route::get('/visualizar-tcc', [
+		'as'=>'visualizar_tcc',
+		'uses'=>'TccController@index',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+		]);
+
+	Route::get('/criar-tcc', [
+		'as'=>'criar_tcc',
+		'uses'=>'TccController@create',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+		]);
+
+	Route::get('/detalhar-tcc/{id}',[
+		'as'=>'detalhar_tcc',
+		'uses'=> 'TccController@show',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+		]);
+
+	Route::post('/salvar-tcc', [
+		'as'=>'salvar_tcc',
+		'uses'=>'TccController@store',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+		]);
+
+	Route::get('/editar-tcc/{id}', [
+		'as'=>'editar_tcc',
+		'uses'=>'TccController@edit',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+		]);
+
+	Route::post('/atualizar-tcc/{id}', [
+		'as'=>'atualizar_tcc',
+		'uses'=>'TccController@update',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+		]);
+});
+
 Route::group(['prefix'=>'usuario'],function(){
 	Route::get('/visualizar-usuario', [
 		'as'=>'visualizar_usuario',
