845b529bde2ca4c63c64032aa3e61eb781d0f0c6
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-12-31 10:57:03 -0200

    Criar atributos dinamicos para a geracao de pesquisas

--- database/factories/PesquisaFactory.php
+++ database/factories/PesquisaFactory.php
@@ -1,21 +1,29 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use PesquisaProjeto\AbordagemPesquisa;
+use PesquisaProjeto\AgenciaPesquisa;
+use PesquisaProjeto\AreaPesquisa;
+use PesquisaProjeto\NaturezaPesquisa;
+use PesquisaProjeto\ObjetivoPesquisa;
+use PesquisaProjeto\ProcedimentosPesquisa;
+use PesquisaProjeto\SubAreaPesquisa;
+use PesquisaProjeto\StatusPesquisa;
 
 $factory->define(PesquisaProjeto\Pesquisa::class, function (Faker $faker) {
+
     return [
     	'pesquisa_titulo'=>$faker->text(20),
     	'pesquisa_resumo'=>$faker->text(200),
     	'pesquisa_ano_inicio'=>date('Y'),
     	'pesquisa_semestre_inicio'=>rand(1,2),
-    	'pesquisa_status'=>rand(1,6),
-    	'natureza_pesquisa_id'=>rand(1,2),
-    	'abordagem_pesquisa_id'=>rand(1,3),
-    	'objetivo_pesquisa_id'=>rand(1,4),
-    	'procedimentos_pesquisa_id'=>rand(1,6),
-    	'area_pesquisa_id'=>rand(1,11),
-    	'agencia_pesquisa_id'=>rand(1,3),
-    	'sub_area_pesquisa_id'=>rand(1,58)
+    	'status_pesquisa_id'=>PesquisaProjeto\StatusPesquisa::inRandomOrder()->first()->id,
+    	'natureza_pesquisa_id'=>PesquisaProjeto\NaturezaPesquisa::inRandomOrder()->first()->id,
+    	'abordagem_pesquisa_id'=>PesquisaProjeto\AbordagemPesquisa::inRandomOrder()->first()->id,
+    	'objetivo_pesquisa_id'=>PesquisaProjeto\ObjetivoPesquisa::inRandomOrder()->first()->id,
+    	'procedimentos_pesquisa_id'=>PesquisaProjeto\ProcedimentosPesquisa::inRandomOrder()->first()->id,
+    	'area_pesquisa_id'=>PesquisaProjeto\AreaPesquisa::inRandomOrder()->first()->id,
+    	'agencia_pesquisa_id'=>PesquisaProjeto\AgenciaPesquisa::inRandomOrder()->first()->id,
+    	'sub_area_pesquisa_id'=>PesquisaProjeto\SubAreaPesquisa::inRandomOrder()->first()->id
     ];
 });
