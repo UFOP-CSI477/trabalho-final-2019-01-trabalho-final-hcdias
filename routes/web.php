fa4a17756c7fd6bb2e41f96c67903d6c464be486
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-12-31 11:37:02 -0200

    Adicionar rotas para deletar tcc e pesquisa

--- routes/web.php
+++ routes/web.php
@@ -25,42 +25,49 @@ Route::group(['prefix'=>'pesquisa'],function(){
 		'uses'=>'PesquisaController@index',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::get('/criar-pesquisa', [
 		'as'=>'criar_pesquisa',
 		'uses'=>'PesquisaController@create',
 		'roles'=>['admin','professor'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::get('/detalhar-pesquisa/{id}',[
 		'as'=>'detalhar_pesquisa',
 		'uses'=> 'PesquisaController@show',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::post('/salvar-pesquisa', [
 		'as'=>'salvar_pesquisa',
 		'uses'=>'PesquisaController@store',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::get('/editar-pesquisa/{id}', [
 		'as'=>'editar_pesquisa',
 		'uses'=>'PesquisaController@edit',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::post('/atualizar-pesquisa/{id}', [
 		'as'=>'atualizar_pesquisa',
 		'uses'=>'PesquisaController@update',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
+
+	Route::get('/delete/{id}', [
+		'as'=>'deletar_pesquisa',
+		'uses'=>'PesquisaController@destroy',
+		'roles'=>['admin','professor'],
+		'middleware'=>'roles'
+	]);
 });
 
 Route::group(['prefix'=>'tcc'],function(){
@@ -69,42 +76,48 @@ Route::group(['prefix'=>'tcc'],function(){
 		'uses'=>'TccController@index',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::get('/criar-tcc', [
 		'as'=>'criar_tcc',
 		'uses'=>'TccController@create',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::get('/detalhar-tcc/{id}',[
 		'as'=>'detalhar_tcc',
 		'uses'=> 'TccController@show',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::post('/salvar-tcc', [
 		'as'=>'salvar_tcc',
 		'uses'=>'TccController@store',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::get('/editar-tcc/{id}', [
 		'as'=>'editar_tcc',
 		'uses'=>'TccController@edit',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
 
 	Route::post('/atualizar-tcc/{id}', [
 		'as'=>'atualizar_tcc',
 		'uses'=>'TccController@update',
 		'roles'=>['admin','professor','aluno'],
 		'middleware'=>'roles'
-		]);
+	]);
+	Route::get('/delete/{id}', [
+		'as'=>'deletar_tcc',
+		'uses'=>'TccController@destroy',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
 });
 
 Route::group(['prefix'=>'usuario'],function(){
