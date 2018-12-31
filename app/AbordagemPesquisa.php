eb2415e035cef350f2230d3a04b2374b5c234805
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-12-31 10:38:30 -0200

    Adicionar relacao entre abordagem, pesquisa e tcc

--- app/AbordagemPesquisa.php
+++ app/AbordagemPesquisa.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class AbordagemPesquisa extends Model
 {
-    //
+    public function pesquisas(){
+    	return $this->hasMany('PesquisaProjeto\Pesquisa');
+    }
+
+    public function tccs(){
+    	return $this->hasMany('PesquisaProjeto\Tcc');
+    }
 }
