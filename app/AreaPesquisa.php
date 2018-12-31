0cf84cc809b1f983b909749a82ba4c881a914e22
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-12-31 10:40:44 -0200

    Adicionar relacao entre area, pesquisa e tcc

--- app/AreaPesquisa.php
+++ app/AreaPesquisa.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class AreaPesquisa extends Model
 {
-    //
+    public function pesquisas(){
+    	return $this->hasMany('PesquisaProjeto\Pesquisa');
+    }
+    public function tccs(){
+    	return $this->hasMany('PesquisaProjeto\Tcc');
+    }    
 }
