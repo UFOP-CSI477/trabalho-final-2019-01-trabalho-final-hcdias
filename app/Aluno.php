c722b4dd557be6737a2f6059f0964c5052578328
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-12-31 10:40:10 -0200

    Adicionar relacionamento entre alunos e pivot banca_tccs

--- app/Aluno.php
+++ app/Aluno.php
@@ -12,6 +12,13 @@ class Aluno extends Model
     	return $this->belongsToMany('PesquisaProjeto\Pesquisa','vinculo_pesquisas')
     		->withPivot('professor_papel_id','aluno_id');
     }
+
+	public function tccs()
+    {
+    	return $this->belongsToMany('PesquisaProjeto\Tcc','banca_tccs')
+    		->withPivot('professor_id','aluno_id');
+    }
+
     public function user(){
     	return $this->hasOne('PesquisaProjeto\VinculoAlunoUser');
     }
