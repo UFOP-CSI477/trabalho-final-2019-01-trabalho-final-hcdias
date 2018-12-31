8ec3c7894a57b24dc2c6ab79f4e22a37298ba343
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-12-31 11:15:22 -0200

    Migrar interface de login
    Interface de login nao possuia responsividade e quebrava com frequencia. Implementando o padrao adminlte.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,119 +1,86 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        
-        <link rel="icon" href="{{ 'favicon.ico' }}">
-            <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('adminlte::master')
 
-        <title>Projetos de Pesquisa</title>
+@section('adminlte_css')
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/auth.css') }}">
+    @yield('css')
+@stop
 
-        <!-- Bootstrap core CSS -->
-        <link href="{{ asset('/vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+@section('body_class', 'login-page')
 
-        <!-- Custom styles for form validation -->
-        <link href="{{ asset('css/form-validation.css') }}" rel="stylesheet">
+@section('body')
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+            <p class="login-box-msg">{{ trans('adminlte::adminlte.login_message') }}</p>
+            <form action="{{ url(config('adminlte.login_url', 'login')) }}" method="post">
+                {!! csrf_field() !!}
 
-        <!-- Custom styles for navbar -->
-        <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
+                <div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+                    <input type="email" name="email" class="form-control" value="{{ old('email') }}"
+                           placeholder="{{ trans('adminlte::adminlte.email') }}">
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <input type="password" name="password" class="form-control"
+                           placeholder="{{ trans('adminlte::adminlte.password') }}">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox" name="remember"> {{ trans('adminlte::adminlte.remember_me') }}
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit"
+                                class="btn btn-primary btn-block btn-flat">{{ trans('adminlte::adminlte.sign_in') }}</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <div class="auth-links">
+                <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}"
+                   class="text-center"
+                >{{ trans('adminlte::adminlte.i_forgot_my_password') }}</a>
+                <br>
+                @if (config('adminlte.register_url', 'register'))
+                    <a href="{{ url(config('adminlte.register_url', 'register')) }}"
+                       class="text-center"
+                    >{{ trans('adminlte::adminlte.register_a_new_membership') }}</a>
+                @endif
+            </div>
+        </div>
+        <!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+@stop
 
-        <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:400,300'>
-        <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/icon?family=Material+Icons'>
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    </head>
-    <body>
-		<div class="bg-light">
-		    <div class="cont_centrar">
-		        <div class="cont_login">
-		            <div class="cont_info_log_sign_up">
-		                <div class="col_md_login">
-		                    <div class="cont_ba_opcitiy">
-		                        <h2>CADASTRO DE PROJETOS</h2>  
-		                        <button class="btn_login" onclick="cambiar_login()">ENTRAR</button>
-		                    </div>
-		                </div>
-
-		                <div class="col_md_sign_up">
-		                    <div class="cont_ba_opcitiy">
-		                        <h2>CONSULTA DE PROJETOS</h2>
-		                        <button class="btn_sign_up" onclick="cambiar_sign_up()">ENTRAR</button>
-		                    </div>
-		                </div>
-		            </div>
-
-		            <div class="cont_back_info">
-		                <div class="cont_img_back_">
-		                    <img src="{{ 'media/icea-ufop.jpg' }}" alt=""/>
-		                </div>
-		            </div>
-
-		       
-		            <div class="cont_forms">
-		                <div class="cont_img_back_">
-		                    <img src="{{ 'media/icea-ufop.jpg' }}" alt=""/>
-		                </div>
-		                
-		                <form method="POST" action="{{ route('login') }}">
-		                    {{ csrf_field() }}
-		                    <div class="cont_form_login">
-		                        <a href="#" onclick="ocultar_login_sign_up()" ><i class="material-icons">&#xE5C4;</i></a>
-		                        <h2>LOGIN</h2>
-
-		                        <input type="text" name="email" placeholder="Email"/>
-		                        <input type="password" name="password" placeholder="Password"/>
-		                        <button class="btn_login"">INICIAR SEÇÃO</button>
-		                    </div>
-		                </form>
-
-		                <form method="POST" action="{{ route('exibir') }}">
-		                    {{ csrf_field() }}
-		                    <div class="cont_form_sign_up">
-		                        <a href="#" onclick="ocultar_login_sign_up()"><i class="material-icons">&#xE5C4;</i></a>
-		                        <h2>LOGIN</h2>
-		                        <input type="text" placeholder="Email"/>
-		                        <input type="text" placeholder="User"/>
-		                        <input type="password" placeholder="Password"/> 
-		                        <button class="btn_sign_up" onclick="window.location.href='index_pic_ufop.php'">INICIAR SEÇÃO</button>
-		                        <button class="btn_sign_up" onclick="window.location.href='localhost:8000/exibir'">ENTRAR COMO ANÔNIMO</button>
-		                    </div>
-		                </form>
-		            </div>
-		        </div>
-		    </div>
-		</div>
-	    <footer class="my-5 pt-5 text-muted text-center text-small">
-	        <p class="mb-1">&copy; 2017-2018 Universidade Federal de Ouro Preto</p>
-	        <ul class="list-inline">
-	            <li class="list-inline-item"><a href="#">Privacidade</a></li>
-	            <li class="list-inline-item"><a href="#">Termos</a></li>
-	            <li class="list-inline-item"><a href="#">Suporte</a></li>
-	        </ul>
-	    </footer>
-	    <script src="{{ asset('js/index.js') }}"></script>
-	    <script src="{{ asset('js/holder.min.js') }}"></script>
-	    <script>
-	    // Starter JavaScript for disabling form submissions if there are invalid fields
-	        (function() {
-	            'use strict';
-	            window.addEventListener('load', function() {
-	                // Fetch all the forms we want to apply custom Bootstrap validation styles to
-	                var forms = document.getElementsByClassName('needs-validation');
-	                // Loop over them and prevent submission
-	                var validation = Array.prototype.filter.call(forms, function(form) {
-	                    form.addEventListener('submit', function(event) {
-	                        if (form.checkValidity() === false) {
-	                            event.preventDefault();
-	                            event.stopPropagation();
-	                        }
-	                        form.classList.add('was-validated');
-	                    }, false);
-	                });
-	            }, false);
-	        })();
-	    </script>
-	</body>
-</html>
+@section('adminlte_js')
+    <script src="{{ asset('vendor/adminlte/plugins/iCheck/icheck.min.js') }}"></script>
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    </script>
+    @yield('js')
+@stop
