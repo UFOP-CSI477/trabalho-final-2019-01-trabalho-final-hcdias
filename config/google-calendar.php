9a2764f0965724bda3758fe17206c8cf21d9faf7
Hugo Carvalho <hugo_root@yahoo.com.br>
2018-12-31 11:39:27 -0200

    Adicionar configuracoes para utilizar a biblioteca google calendar

---
+++ config/google-calendar.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+     * Path to the json file containing the credentials.
+     */
+    'service_account_credentials_json' => storage_path('app/google-calendar/ufop-extensao-9a957b0fb5e9.json'),
+
+    /*
+     *  The id of the Google Calendar that will be used by default.
+     */
+    'calendar_id' => env('GOOGLE_CALENDAR_ID'),
+];
