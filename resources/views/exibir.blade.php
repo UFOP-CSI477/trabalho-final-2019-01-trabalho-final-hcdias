4e05b0ecd2c24b3909dc2c0b5f9d0784858fb609
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-02-07 14:21:10 -0200

    Adicionar template para exibir projetos e tccs

--- resources/views/exibir.blade.php
+++ resources/views/exibir.blade.php
@@ -1,55 +1,102 @@
-<!doctype html>
-<html lang="en">
-  <head>
+
+<!DOCTYPE html>
+<html>
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Gerenciamento de Projetos de Pesquisa</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/vendor/Ionicons/css/ionicons.min.css">
 
-    <link rel="icon" href="favicon.ico">
+            <!-- Select2 -->
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css">
     
-    <title>Consulta de Projetos e Orientações</title>
+    <!-- Theme style -->
+    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/dist/css/AdminLTE.min.css">
+
+            <!-- DataTables -->
+        <link rel="stylesheet" href="//cdn.datatables.net/v/bs/dt-1.10.15/datatables.min.css">
+        <!-- <link rel="stylesheet" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css"> -->
     
-    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+            <!-- iCheck -->
+        <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/plugins/iCheck/square/blue.css"></script>
     
-  </head>
-  
-  <body class="bg-light">
+        <link rel="stylesheet"
+          href="http://localhost:8000/vendor/adminlte/dist/css/skins/skin-red.min.css ">
+        
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-<!-- Start Navbar -->
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-red sidebar-mini ">
 
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">OPÇÕES DE CONSULTA</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <div class="wrapper">
 
-      <div class="collapse navbar-collapse" id="navbarsExample01">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Projetos de Iniciação Científica <span class="sr-only">(current)</span></a>
-          </li>
-<!--
-          <li class="nav-item">
-            <a class="nav-link" href="#">Trabalhos de Conclusão de Curso</a>
-          </li>
--->
-        </ul>
-      </div>
-    </nav>
+        <!-- Main Header -->
+        <header class="main-header">
+                        <!-- Logo -->
+            <a href="http://localhost:8000/home" class="logo">
+                <!-- mini logo for sidebar mini 50x50 pixels -->
+                <span class="logo-mini"><b>D</b>NP</span>
+                <!-- logo for regular state and mobile devices -->
+                <span class="logo-lg"><b>UFOP</b>-DEENP</span>
+            </a>
 
-<!-- End Navbar -->
+            <!-- Header Navbar -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <!-- <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button"> -->
+                    <!-- <span class="sr-only">Toggle navigation</span> -->
+                <!-- </a> -->
+                            <!-- Navbar Right Menu -->
+                <div class="navbar-custom-menu">
 
-  <br>
-    <div class="container">
-      <div class="py-5 text-center">
-        <img class="d-block mx-auto mb-4" src="http://www.sisbin.ufop.br/evento/images/ufop.gif" alt="" width="100">
-        <h2>Projetos de Iniciação Científica</h2>
-        <p class="lead">Projetos de Iniciação Científica cadastrados e ativos no Departamento de Engenharia de Produção no Instituto de Ciências Exatas e Aplicadas da Universidade Federal de Ouro Preto</p>
-      </div>
-    </div>
-<!--
-    <div class="col-md-12 order-md-1">
+                    <!-- <ul class="nav navbar-nav">
+                        <li>
+                                                            <a href="#"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                                >
+                                    <i class="fa fa-fw fa-power-off"></i> Log Out
+                                </a>
+                                <form id="logout-form" action="http://localhost:8000/logout" method="POST" style="display: none;">
+                                                                        <input type="hidden" name="_token" value="Q3QFjqKlv833CokbSsZEK4JfPvn58lzyOEPy9NHc">
+                                </form>
+                                                    </li>
+                    </ul> -->
+                </div>
+                            </nav>
+        </header>
+        <div class="content-wrapper" style="margin-left:0px">
+            
+            <!-- Content Header (Page header) -->
+            <!-- <section class="content-header">
+                    <h1>Bem vindo, admin</h1>
+    <small>Acopanhe sua movimentação recente de projetos</small>
+            </section> -->
+
+            <!-- Main content -->
+            <section class="content">
+              <div class="row">
+                <div class="container">
+                  <div class="py-5 text-center">
+                    <img class="d-block mx-auto mb-4" src="http://www.sisbin.ufop.br/evento/images/ufop.gif" alt="" width="100">
+                    <h2>Projetos de Iniciação Científica</h2>
+                    <p class="lead">Projetos de Iniciação Científica cadastrados e ativos no Departamento de Engenharia de Produção no Instituto de Ciências Exatas e Aplicadas da Universidade Federal de Ouro Preto</p>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 order-md-1">
       <hr class="mb-4">
         <h4 class="mb-3">Filtros de Pesquisa</h4>
 
@@ -57,7 +104,7 @@
 
           <div class="col-md-3 mb-3">
               <label for="firstName">Professor orientador</label>
-                <select class="custom-select d-block w-100" id="country" required>
+                <select class="form-control" id="country" required>
                   <option value="">Todos...</option>
                     <option>Marco Antonio Bonelli Junior</option>
             <option>Mônica do Amaral</option>
@@ -70,7 +117,7 @@
 
           <div class="col-md-3 mb-3">
               <label for="firstName">Status do projeto</label>
-                <select class="custom-select d-block w-100" id="country" required>
+                <select class="form-control" id="country" required>
                   <option value="">Todos...</option>
                     <option>Em concepção</option>
             <option>Em desenvolvimento</option>
@@ -83,7 +130,7 @@
 
           <div class="col-md-3 mb-3">
               <label for="firstName">Área de conhecimento</label>
-                <select class="custom-select d-block w-100" id="country" required>
+                <select class="form-control" id="country" required>
                   <option value="">Todos...</option>
                     <option>Logística e cadeia de suprimentos</option>
             <option>Pesquisa operacional</option>
@@ -96,7 +143,7 @@
 
           <div class="col-md-3 mb-3">
               <label for="firstName">Abordagem de pesquisa</label>
-                <select class="custom-select d-block w-100" id="country" required>
+                <select class="form-control" id="country" required>
                   <option value="">Todos...</option>
                     <option>Quantitativo</option>
             <option>Qualitativo</option>
@@ -107,105 +154,71 @@
             </div>
 
       </div>
--->
-      <hr class="mb-4">
 
-      <div id="accordion">
-        <br>
+              </div>
 
-        <?php
+</div>
+            <div class="row" style="margin-top: 35px">
+              <?php for($cont = 0; $cont < 10; $cont++): ?>
+              <div class="col-md-3">
+          <div class="box box-solid">
+            <div class="box-header with-border">
+              <i class="fa fa-text-gear"></i>
 
-          $conexao = new PDO("mysql:host=localhost;dbname=homestead", "homestead", "secret"); 
-        
-          $query = "SELECT  a.*, b.*,c.*,d.descricao as natureza,e.descricao as abordagem,f.descricao as objetivo FROM pesquisas a 
-          join vinculo_pesquisas b ON a.id = b.pesquisa_id 
-          join professores c ON b.professor_id = c.id 
-          join natureza_pesquisas d on a.natureza_pesquisa_id = d.id
-          join abordagem_pesquisas e on a.abordagem_pesquisa_id = e.id
-          join objetivo_pesquisas f on a.objetivo_pesquisa_id = f.id
-          where b.professor_papel_id = 1
-          group by b.pesquisa_id";
-      
-          $statement = $conexao->prepare($query);
-          $statement->execute();
-          $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
-          // dd($result);die;
-          foreach($result as $res){
-        
-            $tabela = '<div class="card">';
-            $tabela .= '<div class="card-header" id="heading' . $res['pesquisa_id'] . '">';
-            $tabela .= '<h5 class "mb-0">';
-            $tabela .= '<button class="btn btn-link text-dark" color="red" data-toggle="collapse" data-target="#collapse' . $res['pesquisa_id'] . '" aria-expanded="false" aria-controls="collapse' . $res['pesquisa_id'] . '">';
-            $tabela .= utf8_encode($res['pesquisa_titulo']);
-            $tabela .= '</button>';
-            $tabela .= '</h5>';
-            $tabela .= '</div>';
-            $tabela .= '<div id="collapse' . $res['pesquisa_id'] . '" class="collapse" aria-labelledby="heading' . $res['pesquisa_id'] . '" data-parent="#accordion">';
-            $tabela .= '<div class="card-body">' . 
-                        '<b>Nome do professor:</b> ' . $res['professor_nome'] . '<br>' .
-                        '<b>Quantidade de alunos:</b> -- <br>' .
-                        '<hr class="mb-4">' .
-                        '<b>Natureza da pesquisa:</b> ' . utf8_encode($res['natureza']) . '<br>' .
-                        '<b>Abordagem da pesquisa:</b> ' . $res['abordagem'] . '<br>' .
-                        '<b>Objetivo da pesquisa:</b> ' . $res['objetivo'] . '<br>' .
-                        '<hr class="mb-4">' .
-                        '<b>Inicio do trabalho:</b> ' . $res['pesquisa_semestre_inicio'] . ' de ' . $res['pesquisa_ano_inicio'] . '<br>' .
-                        '<b>Status de desenvolvimento:</b> ' . $res['pesquisa_status'] . '<br>' .
-                        '<hr class="mb-4">' .
-                        '<b>Resumo do projeto:</b> ' . utf8_encode($res['pesquisa_resumo']) . '<br>' .
-                        '</div>';
-            $tabela .= '</div>';
-            $tabela .= '</div>';
-    
-            echo $tabela . '<br>';
-          }
-        ?>
-    
-    <!-- <p><strong>Note:</strong> The <strong>data-parent</strong> attribute makes sure that all collapsible elements under the specified parent will be closed when one of the collapsible item is shown.</p> <!-- -->
-    
-      </div> 
-  
-      <div>
-        <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">&copy; 2017-2018 Universidade Federal de Ouro Preto</p>
-        <ul class="list-inline">
-          <li class="list-inline-item"><a href="#">Privacidade</a></li>
-          <li class="list-inline-item"><a href="#">Termos</a></li>
-          <li class="list-inline-item"><a href="#">Suporte</a></li>
-        </ul>
-        </footer>
-      </div>
+              <h3 class="box-title">Descrição do projeto</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <dl class="dl-horizontal">
+                <dt>
+                      <img src="/media/mario.png">
+                    
+                    
+                </dt>
+                <dd>Descrição do projeto. Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
+                Donec id elit non mi porta gravida at eget metus.</dd>
+                
+                <dt>Contato</dt>
+                <dd>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
+                  sit amet risus.
+                </dd>
+              </dl>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+      <?php endfor; ?>
+
+            </div>
+
+            </section>
+            <!-- /.content -->
+                    </div>
+        <!-- /.content-wrapper -->
+
     </div>
+    <!-- ./wrapper -->
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="bootstrap/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="assets/js/vendor/popper.min.js"></script>
-    <script src="dist/js/bootstrap.min.js"></script>
-    <script src="assets/js/vendor/holder.min.js"></script>
-    <script>
-      // Example starter JavaScript for disabling form submissions if there are invalid fields
-      (function() {
-        'use strict';
+<script src="http://localhost:8000/vendor/adminlte/vendor/jquery/dist/jquery.min.js"></script>
+<script src="http://localhost:8000/vendor/adminlte/vendor/jquery/dist/jquery.slimscroll.min.js"></script>
+<script src="http://localhost:8000/vendor/adminlte/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 
-        window.addEventListener('load', function() {
-          // Fetch all the forms we want to apply custom Bootstrap validation styles to
-          var forms = document.getElementsByClassName('needs-validation');
+    <!-- Select2 -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 
-          // Loop over them and prevent submission
-          var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add('was-validated');
-            }, false);
-          });
-        }, false);
-      })();
-    </script>
-  </body>
+    <!-- DataTables -->
+    <!-- <script src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script> -->
+    <script src="//cdn.datatables.net/v/bs/dt-1.10.15/datatables.min.js"></script>
+
+    <!-- ChartJS -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
+
+    <!-- iCheck -->
+    <script src="http://localhost:8000/vendor/adminlte/plugins/iCheck/icheck.min.js"></script>
+
+    <script src="http://localhost:8000/vendor/adminlte/dist/js/adminlte.min.js"></script>
+        
+</body>
 </html>
+
