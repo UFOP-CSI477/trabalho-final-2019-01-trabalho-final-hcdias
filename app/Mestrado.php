cb5096c69eb4e0a8c1da7b3cbe503eeec9796607
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-03-27 19:06:21 -0300

    Criar atributos e relacoes para model Mestrado

--- app/Mestrado.php
+++ app/Mestrado.php
@@ -6,5 +6,63 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mestrado extends Model
 {
-    //
+    protected $fillable = [
+		'titulo_mestrado',
+		'resumo_mestrado',
+		'ano_inicio_mestrado',
+		'semestre_inicio_mestrado',
+		'semestre_defesa_mestrado',
+		'status_mestrado',
+		'sisbin_mestrado',
+		'abordagem_mestrado_id',
+		'area_mestrado_id',
+		'natureza_mestrado_id',
+		'objetivo_mestrado_id',
+		'procedimentos_mestrado_id',
+		'sub_area_mestrado_id',
+		'orientador_mestrado_id',
+		'coorientador_mestrado_id',
+		'aluno_mestrado_id'
+		];
+
+	public function orientador(){
+		return $this->belongsTo('PesquisaProjeto\Professor','orientador_mestrado_id');
+	}
+
+	public function coorientador(){
+		return $this->belongsTo('PesquisaProjeto\Professor','coorientador_mestrado_id');
+	}
+
+    public function aluno(){
+    	return $this->belongsTo('PesquisaProjeto\Aluno','aluno_mestrado_id');
+    }
+
+	public function abordagem(){
+    	return $this->belongsTo('PesquisaProjeto\AbordagemPesquisa','abordagem_mestrado_id');
+    }
+
+
+    public function area(){
+    	return $this->belongsTo('PesquisaProjeto\AreaPesquisa','area_mestrado_id');
+    }
+
+    public function natureza(){
+    	return $this->belongsTo('PesquisaProjeto\NaturezaPesquisa','natureza_mestrado_id');
+    }
+
+    public function objetivo(){
+    	return $this->belongsTo('PesquisaProjeto\ObjetivoPesquisa','objetivo_mestrado_id');
+    }
+
+    public function procedimento(){
+    	return $this->belongsTo('PesquisaProjeto\ProcedimentosPesquisa','procedimentos_mestrado_id');
+    }
+
+    public function status(){
+    	return $this->belongsTo('PesquisaProjeto\StatusPesquisa','status_mestrado');
+    }
+
+    public function subarea(){
+    	return $this->belongsTo('PesquisaProjeto\SubAreaPesquisa','sub_area_mestrado_id');
+    }
 }
