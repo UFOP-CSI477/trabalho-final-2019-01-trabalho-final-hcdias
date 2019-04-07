a50b7460f1e676402a33919641ab89f3a10efc92
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-04-07 10:50:31 -0300

    Adicionar rotas para modulo de cadastro de extensao

--- routes/web.php
+++ routes/web.php
@@ -250,6 +250,55 @@ Route::group(['prefix'=>'mestrado'],function(){
 	]);
 });
 
+Route::group(['prefix'=>'extensao'],function(){
+	Route::get('/visualizar-extensao', [
+		'as'=>'visualizar_extensao',
+		'uses'=>'ExtensaoController@index',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
+
+	Route::get('/criar-extensao', [
+		'as'=>'criar_extensao',
+		'uses'=>'ExtensaoController@create',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
+
+	Route::get('/detalhar-extensao/{id}',[
+		'as'=>'detalhar_extensao',
+		'uses'=> 'ExtensaoController@show',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
+
+	Route::post('/salvar-extensao', [
+		'as'=>'salvar_extensao',
+		'uses'=>'ExtensaoController@store',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
+
+	Route::get('/editar-extensao/{id}', [
+		'as'=>'editar_extensao',
+		'uses'=>'ExtensaoController@edit',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
+
+	Route::post('/atualizar-extensao/{id}', [
+		'as'=>'atualizar_extensao',
+		'uses'=>'ExtensaoController@update',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
+	Route::get('/delete/{id}', [
+		'as'=>'deletar_tcc',
+		'uses'=>'extensaoController@destroy',
+		'roles'=>['admin','professor','aluno'],
+		'middleware'=>'roles'
+	]);
+});
 Route::group(['prefix'=>'exibir'],function(){
 Route::post('/exibir-resultados', [
 		'as'=>'exibir_resultados',
