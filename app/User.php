c8aee5fdff40cba45c146c446aea305a1ed5c7f0
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-20 00:29:44 -0300

    Adicionar relacoes entre usuario e Pesquisa,TCC e Mestrado

--- app/User.php
+++ app/User.php
@@ -37,6 +37,46 @@ class User extends Authenticatable
         return $this->hasOne('PesquisaProjeto\VinculoUser','app_user_id');
     }
 
+    public function professorPesquisas()
+    {
+        return $this->hasMany('PesquisaProjeto\Pesquisa','orientador_id')
+        ->orWhere('coorientador_id',$this->id);
+    }
+
+    public function alunoPesquisas()
+    {
+        return $this->belongsToMany('PesquisaProjeto\Pesquisa','vinculo_pesquisas','user_id','pesquisa_id');
+
+    }    
+
+    public function professorTccs()
+    {
+        return $this->hasMany('PesquisaProjeto\Tcc','orientador_tcc_id')
+        ->orWhere('coorientador_tcc_id',$this->id);
+    }
+
+    public function professorMestrados()
+    {
+        return $this->hasMany('PesquisaProjeto\Mestrado','orientador_mestrado_id')
+        ->orWhere('coorientador_mestrado_id',$this->id);
+    }
+
+    public function alunoMestrados()
+    {
+        return $this->hasMany('PesquisaProjeto\Mestrado','aluno_mestrado_id');
+    }    
+    
+    public function alunoTccs()
+    {
+        return $this->hasOne('PesquisaProjeto\Tcc','aluno_tcc_id','id');
+    }
+
+    public function bancaTccs()
+    {
+        return $this->belongsToMany('PesquisaProjeto\Tcc','banca_tccs')
+            ->withPivot('professor_id','aluno_id','status');
+    }    
+
     public function hasAnyRole($roles)
     {
 
