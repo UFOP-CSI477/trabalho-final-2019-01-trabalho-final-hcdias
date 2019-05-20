5fc63ccb9746c4c418f5079434d691539c0a1d97
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-20 00:37:17 -0300

    Alterar relacao de atributos utilizados na geracao de vinculos de pesquisa fake
    Relacao entre pesquisa e autor foi alterada para se basear apenas no usuario e nao mais em professor, professor_papel e alunos. A partir desta alteracao apenas o usuario e necessario.

--- database/factories/VinculoPesquisaFactory.php
+++ database/factories/VinculoPesquisaFactory.php
@@ -5,9 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(PesquisaProjeto\VinculoPesquisa::class, function (Faker $faker) {
     return [
-    	'pesquisa_id'=>mt_rand(1,5),
-    	'professor_papel_id'=>mt_rand(1,2),
-    	'aluno_id'=>mt_rand(1,10),
-    	'professor_id'=>mt_rand(1,5)
+    	'pesquisa_id'=>PesquisaProjeto\Pesquisa::inRandomOrder()->first()->id,
+    	'user_id'=>mt_rand(1,3),
     ];
 });
