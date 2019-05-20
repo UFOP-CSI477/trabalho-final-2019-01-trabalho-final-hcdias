0982411b2a084855ca26438a65b88a71cc95cbfa
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-20 01:48:34 -0300

    Vincular roles a grupos predefinidos de acordo com o papel do usuario

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use PesquisaProjeto\Role;
+use PesquisaProjeto\Group;
 
 class RoleSeeder extends Seeder
 {
@@ -12,19 +13,35 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        $role_user = new PesquisaProjeto\Role();
-        $role_user->name = "professor";
-        $role_user->description = "Normal user,basic permissions";
-        $role_user->save();
+
+        $group_admin = Group::where('codigo',1)->first();
+        $group_coordenador = Group::where('codigo',2)->first();
+        $group_professor = Group::whereIn('codigo',[714,715,716,71481,71130])->get();
+        $group_aluno = Group::whereIn('codigo',[7215,7213,7217,7236])->get();
+        
+
+        $role_professor = new PesquisaProjeto\Role();
+        $role_professor->name = "professor";
+        $role_professor->description = "Normal user,basic permissions";
+        $role_professor->save();
+        //$role_professor->groups()->attach($group_professor);
 
         $role_aluno = new PesquisaProjeto\Role();
         $role_aluno->name = "aluno";
         $role_aluno->description = "Normal user,basic permissions";
         $role_aluno->save();
+        //$role_aluno->groups()->attach($group_aluno);
 
         $role_admin = new PesquisaProjeto\Role();
         $role_admin->name = "admin";
         $role_admin->description = "Admin user,all permissions";
         $role_admin->save();
+       // $role_admin->groups()->attach($group_admin);
+
+        $role_coordenador = new PesquisaProjeto\Role();
+        $role_coordenador->name = "coordenador";
+        $role_coordenador->description = "Coodenador user,read permissions";
+        $role_coordenador->save();
+        //$role_coordenador->groups()->attach($group_coordenador);
     }
 }
