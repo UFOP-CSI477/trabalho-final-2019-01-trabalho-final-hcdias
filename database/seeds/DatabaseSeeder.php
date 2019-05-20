adb78414213429240b1dde572c078a874c312f09
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-20 01:45:41 -0300

    Adicionar seeder para usuarios MinhaUfop

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,22 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(DepartamentosSeeder::class);
-         $this->call(CursoSeeder::class);
-         $this->call(AlunoSeeder::class);
-         $this->call(ProfessorSeeder::class);
-         $this->call(NaturezaPesquisaSeeder::class);
-         $this->call(AbordagemPesquisaSeeder::class);
-         $this->call(ObjetivoPesquisaSeeder::class);
-         $this->call(ProcedimentosPesquisaSeeder::class);
-         $this->call(AreaPesquisaSeeder::class);
-         $this->call(AgenciaPesquisaSeeder::class);
-         $this->call(SubAreaPesquisaSeeder::class);
-         $this->call(StatusPesquisaSeeder::class);
-         $this->call(PesquisaSeeder::class);
-         $this->call(ProfessorPapelSeeder::class);
-         $this->call(VinculoPesquisaSeeder::class);
-         $this->call(RoleSeeder::class);
-         $this->call(UserSeeder::class);
+        $this->call(DepartamentosSeeder::class);
+        $this->call(CursoSeeder::class);
+        $this->call(AlunoSeeder::class);
+        $this->call(ProfessorSeeder::class);
+        $this->call(NaturezaPesquisaSeeder::class);
+        $this->call(AbordagemPesquisaSeeder::class);
+        $this->call(ObjetivoPesquisaSeeder::class);
+        $this->call(ProcedimentosPesquisaSeeder::class);
+        $this->call(AreaPesquisaSeeder::class);
+        $this->call(AgenciaPesquisaSeeder::class);
+        $this->call(SubAreaPesquisaSeeder::class);
+        $this->call(StatusPesquisaSeeder::class);
+        $this->call(PesquisaSeeder::class);
+        $this->call(ProfessorPapelSeeder::class);
+        $this->call(VinculoPesquisaSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(GroupSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(MinhaUfopUserSeeder::class);
     }
 }
