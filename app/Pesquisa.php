ccf65f378ce9b29b7894d7447b911cb6f039bac5
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-20 00:22:49 -0300

    Alterar relacao de papeis de usuario e alterar semantica de atributos
    Remover relacao com as entidades Professor e Aluno e adicionar relacao para a entidade MinhaUfopUser. Melhorar a semantica dos atributos relacionais, removendo o trecho 'pesquisa' dos atributos.

--- app/Pesquisa.php
+++ app/Pesquisa.php
@@ -7,59 +7,63 @@ use Illuminate\Database\Eloquent\Model;
 class Pesquisa extends Model
 {
 	protected $fillable = [
-		'pesquisa_titulo',
-		'pesquisa_resumo',
-		'pesquisa_ano_inicio',
-		'pesquisa_semestre_inicio',
-		'status_pesquisa_id',
-		'abordagem_pesquisa_id',
-		'agencia_pesquisa_id',
-		'area_pesquisa_id',
-		'natureza_pesquisa_id',
-		'objetivo_pesquisa_id',
-		'procedimentos_pesquisa_id',
-		'sub_area_pesquisa_id'
+		'titulo',
+		'resumo',
+		'ano_inicio',
+		'semestre_inicio',
+		'status_id',
+		'abordagem_id',
+		'agencia_id',
+		'area_id',
+		'natureza_id',
+		'objetivo_id',
+		'procedimentos_id',
+		'sub_area_id',
+        'orientador_id',
+        'coorientador_id'
 		];
 	 
-    public function professores(){
-    	return $this->belongsToMany('PesquisaProjeto\Professor','vinculo_pesquisas')
-    		->withPivot('professor_papel_id','aluno_id');
+    public function orientador(){
+    	return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','orientador_id');
+    }
+
+    public function coorientador(){
+        return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','coorientador_id');
     }
 
     public function alunos(){
-    	return $this->belongsToMany('PesquisaProjeto\Aluno','vinculo_pesquisas')
-    		->withPivot('professor_papel_id','aluno_id');
+    	return $this->belongsToMany('PesquisaProjeto\MinhaUfopUser','vinculo_pesquisas','pesquisa_id','user_id');
     }
 
 	public function abordagem(){
-    	return $this->belongsTo('PesquisaProjeto\AbordagemPesquisa','abordagem_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\AbordagemPesquisa','abordagem_id');
     }
 
 	public function agencia(){
-    	return $this->belongsTo('PesquisaProjeto\AgenciaPesquisa','agencia_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\AgenciaPesquisa','agencia_id');
     }
 
     public function area(){
-    	return $this->belongsTo('PesquisaProjeto\AreaPesquisa','area_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\AreaPesquisa','area_id');
     }
 
     public function natureza(){
-    	return $this->belongsTo('PesquisaProjeto\NaturezaPesquisa','natureza_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\NaturezaPesquisa','natureza_id');
     }
 
     public function objetivo(){
-    	return $this->belongsTo('PesquisaProjeto\ObjetivoPesquisa','objetivo_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\ObjetivoPesquisa','objetivo_id');
     }
 
     public function procedimento(){
-    	return $this->belongsTo('PesquisaProjeto\ProcedimentosPesquisa','procedimentos_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\ProcedimentosPesquisa','procedimentos_id');
     }
 
     public function status(){
-    	return $this->belongsTo('PesquisaProjeto\StatusPesquisa','status_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\StatusPesquisa','status_id');
     }
 
     public function subarea(){
-    	return $this->belongsTo('PesquisaProjeto\SubAreaPesquisa','sub_area_pesquisa_id');
+    	return $this->belongsTo('PesquisaProjeto\SubAreaPesquisa','sub_area_id');
     }
 }
