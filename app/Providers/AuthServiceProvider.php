461d965b70b6ce3160397fa5a95b1a1367224082
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-20 02:26:05 -0300

    Adicionar gates para verificar tipo de usuario. Adicionar custom provider para autenticacao

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace PesquisaProjeto\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use \Illuminate\Support\Facades\Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,8 +26,35 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Auth::provider('minhaufopuserprovider', function($app, array $config) {
+            return new MinhaUfopUserProvider($app['hash'],$config['model']);
+        });       
+
         Gate::define('show-menu',function($user,$requiredPermission){
-           return in_array($user->roles->first()->name,$requiredPermission);
+           $roles = $user->roles;
+           foreach($roles as $role){
+            if(in_array($role->name,$requiredPermission)){
+                return true;
+            }
+           }
+           
+           return false;
+        });
+
+        Gate::define('has-actor',function($user){
+            return $user->vinculo()->first() !== null;
+        });
+
+        Gate::define('admin',function($user){
+            return $user->hasRole('admin');
+        });
+
+        Gate::define('professor',function($user){
+            return $user->hasRole('professor');
+        });
+
+        Gate::define('aluno',function($user){
+            return $user->hasRole('aluno');
         });
     }
 }
