8e03017a7d80178f4636b6e4f891d9c33c227220
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-20 02:32:54 -0300

    Adicionar model MinhaUfopUser

---
+++ app/MinhaUfopUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PesquisaProjeto;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use PesquisaProjeto\User;
+class MinhaUfopUser extends User
+{
+
+    protected $guard = 'minhaufop-guard';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'cpf','id_grupo',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'remember_token','cpf','id_grupo'
+    ];
+}
