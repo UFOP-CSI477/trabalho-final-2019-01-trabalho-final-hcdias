a8773ad4e0e7fe26cd832084511494d730e88a08
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-05-21 12:33:09 -0300

    Alterar caminho de assets estaticos para dinamicos de acordo com o fw

--- resources/views/exibir.blade.php
+++ resources/views/exibir.blade.php
@@ -8,27 +8,25 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.min.css') }}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/font-awesome/css/font-awesome.min.css') }}">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/vendor/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/Ionicons/css/ionicons.min.css') }}">
 
-            <!-- Select2 -->
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css">
+    <!-- Select2 -->
+    <!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css"> -->
     
     <!-- Theme style -->
-    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="{{ asset('/vendor/adminlte/dist/css/AdminLTE.min.css')}}">
 
-            <!-- DataTables -->
-        <link rel="stylesheet" href="//cdn.datatables.net/v/bs/dt-1.10.15/datatables.min.css">
-        <!-- <link rel="stylesheet" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css"> -->
+    <!-- DataTables -->
+    <link rel="stylesheet" href="//cdn.datatables.net/v/bs/dt-1.10.15/datatables.min.css">
     
-            <!-- iCheck -->
-        <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/plugins/iCheck/square/blue.css"></script>
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('/vendor/adminlte/plugins/iCheck/square/blue.css')}}"></script>
     
-        <link rel="stylesheet"
-          href="http://localhost:8000/vendor/adminlte/dist/css/skins/skin-red.min.css ">
+    <link rel="stylesheet" href="{{ asset('/vendor/adminlte/dist/css/skins/skin-red.min.css')}} ">
         
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -37,6 +35,7 @@
 
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <meta name="google-site-verification" content="rXsxNrf-YuK-uYxeY2YcmSL15AJPC9UQBBwcvNZOwOw" />
 </head>
 <body class="hold-transition skin-red sidebar-mini ">
 
@@ -49,7 +48,7 @@
                 <!-- mini logo for sidebar mini 50x50 pixels -->
                 <span class="logo-mini"><b>D</b>NP</span>
                 <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>UFOP</b>-DEENP</span>
+                <span class="logo-lg">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</span>
             </a>
 
             <!-- Header Navbar -->
@@ -61,20 +60,8 @@
                             <!-- Navbar Right Menu -->
                 <div class="navbar-custom-menu">
 
-                    <!-- <ul class="nav navbar-nav">
-                        <li>
-                                                            <a href="#"
-                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                                >
-                                    <i class="fa fa-fw fa-power-off"></i> Log Out
-                                </a>
-                                <form id="logout-form" action="http://localhost:8000/logout" method="POST" style="display: none;">
-                                                                        <input type="hidden" name="_token" value="Q3QFjqKlv833CokbSsZEK4JfPvn58lzyOEPy9NHc">
-                                </form>
-                                                    </li>
-                    </ul> -->
                 </div>
-                            </nav>
+              </nav>
         </header>
         <div class="content-wrapper" style="margin-left:0px">
             
@@ -105,14 +92,14 @@
       <div class="row">
           <div class="col-md-3 mb-3">
               <label for="firstName">Professor orientador</label>
-                <select class="form-control" name="professor_id" required>
+                <select class="form-control" name="professor_id" >
                     <option value="">Todos...</option>
                     @foreach($professores as $professor)
-                      <option value="{{$professor->id}}">{{$professor["nome"]}}</option>
+                      <option value="{{$professor->id}}">{{$professor->name}}</option>
                     @endforeach
                 </select>
               <div class="invalid-feedback">
-                  Favor informar professor válido.
+                  
             </div>
             </div>
     
@@ -120,45 +107,52 @@
 
           <div class="col-md-3 mb-3">
               <label for="firstName">Status do projeto</label>
-                <select class="form-control" name="status_id" required>
+                <select class="form-control" name="status_id" >
                   <option value="">Todos...</option>
                   @foreach($status as $estado)
                     <option value="{{$estado->id}}">{{$estado["descricao"]}}</option>
                   @endforeach
                 </select>
               <div class="invalid-feedback">
-                  Favor informar status válido.
+                  
             </div>
             </div>
             
           <div class="col-md-3 mb-3">
               <label for="firstName">Área de conhecimento</label>
-                <select class="form-control" name="areaPesquisa_id" required>
+                <select class="form-control" name="areaPesquisa_id" >
                   <option value="">Todos...</option>
                   @foreach($areasPesquisa as $areas)
                     <option value="{{$areas->id}}">{{$areas["descricao"]}}</option>
                   @endforeach
                 </select>
               <div class="invalid-feedback">
-                  Favor informar área válida.
+                  
             </div>
             </div>
             
           <div class="col-md-3 mb-3">
               <label for="firstName">Abordagem de pesquisa</label>
-                <select class="form-control" name="abordagem_id" required>
+                <select class="form-control" name="abordagem_id" >
                   <option value="">Todos...</option>
                   @foreach($abordagens as $abordagem)
                     <option value="{{$abordagem->id}}">{{$abordagem["descricao"]}}</option>
                   @endforeach    
                 </select>
               <div class="invalid-feedback">
-                  Favor informar abordagem válida.
+                  
             </div>
             </div>
 
       </div>
-      <button type="submit" class="btn btn-block btn-primary btn-lg">Pesquisar</button>
+      <div class="row" style="margin-top: 10px">
+        <div class="col-md-4"></div>
+        
+        <div class="col-md-3">
+          <button type="submit" class="btn btn-block btn-primary btn-lg">Pesquisar</button>
+        </div>
+      <div class="col-md-4"></div>
+      </div>
       </form>
               </div>
 
