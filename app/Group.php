46923177090d3590605a694bd4405a8981bac99b
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-06-07 14:24:39 -0300

    Adicionar relacao entre grupos e users
    Cada usuario pertence a um grupo, que por sua vez pertence possui um papel. Configurando a relacao.

--- app/Group.php
+++ app/Group.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    public function users()
+    {
+    	return $this->hasMany('PesquisaProjeto\User');
+    }
+
     public function roles()
     {
     	return $this->belongsTo('PesquisaProjeto\Role');
-    }
+    }    
 }
