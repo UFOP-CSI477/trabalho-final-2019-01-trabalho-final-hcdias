66ff994ebe24dc43f9e582c7c5ca26d6264de9cf
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-06-17 00:31:16 -0300

    Adicionar migration para model Group

---
+++ database/migrations/2018_05_31_030744_create_groups_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigo');
+            $table->string('descricao');
+            $table->integer('roles_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('roles_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
