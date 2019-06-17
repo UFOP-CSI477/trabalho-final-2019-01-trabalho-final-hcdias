29e82097f50cbb4512e2fdced97c6d4d0f96cb91
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-06-17 00:24:19 -0300

    Adicionar controller para usuarios via minhaUFOP

---
+++ app/Http/Controllers/Auth/MinhaUfopLoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PesquisaProjeto\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use PesquisaProjeto\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class MinhaUfopLoginController extends Controller
+{
+
+	use AuthenticatesUsers;
+
+	/**
+	* Redirecionamento apos o login
+	*
+	* @var string
+	*/
+	protected $redirectTo = '/home';
+
+    /**
+     * Exibe o form de login minhaufop
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginUfop()
+    {
+    	return view('authminhaufop.login');
+    }
+
+    /**
+     * Guard do controller
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function guard()
+    {
+    	return Auth::guard('minhaufop-guard');
+    }
+
+    public function __construct()
+    {
+        $this->middleware('guest:minhaufop-guard')->except('logout');
+    }
+}
