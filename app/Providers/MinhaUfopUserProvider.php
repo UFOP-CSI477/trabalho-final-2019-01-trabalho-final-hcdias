d8dbfdb8c9b0120ceca8b2c44b1c60a226a1a660
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-06-17 00:28:48 -0300

    Adicionar UserProvider para a autenticacao via minhaUFOP

---
+++ app/Providers/MinhaUfopUserProvider.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace PesquisaProjeto\Providers;
+
+use PesquisaProjeto\LdapiAPI\LdapiAPIFacade;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+
+
+class MinhaUfopUserProvider implements UserProvider {
+
+	public function retrieveByToken($identifier, $token){
+
+	}
+
+	public function updateRememberToken(Authenticatable $user, $token){
+
+	}
+
+    /**
+     * Retrieve a user by the given credentials.
+     *
+     * @param  array  $credentials
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByCredentials(array $credentials)
+    {
+        if (empty($credentials) ||
+           (count($credentials) === 1 &&
+            array_key_exists('password', $credentials))) {
+            return;
+        }
+        
+        $attributes = LdapiAPIFacade::authUserAPI($credentials);
+   		
+   		if(is_null($attributes)) return null;
+
+        $minhaUfopModel = $this->createDynamicModel(\PesquisaProjeto\MinhaUfopUser::class);
+
+        $query = $minhaUfopModel->newQuery();
+        $result = $query->where('cpf',$credentials['email'])->first();
+        
+        if( !is_null($result) ) return $result;
+
+        if( is_null($result) ){
+        	$groupModel = $this->createDynamicModel(\PesquisaProjeto\Group::class);
+        	$groupUser = $groupModel
+        	->where('codigo',$attributes['id_grupo'])
+        	->first();
+
+        	$minhaUfopModel->name = $attributes['nome'];
+        	$minhaUfopModel->email = $attributes['email'];
+        	$minhaUfopModel->cpf = $attributes['cpf'];
+        	$minhaUfopModel->group_id = $groupUser->id;
+        	$minhaUfopModel->save();
+        }
+
+        return $minhaUfopModel;
+    }
+
+
+    
+
+    public function createDynamicModel($model)
+    {
+        $class = '\\'.ltrim($model, '\\');
+
+        return new $class;
+    }    
+
+    public function validateCredentials(UserContract $user, array $credentials)
+    {
+       return true;
+    }    
+
+    /**
+     * Retrieve a user by their unique identifier.
+     *
+     * @param  mixed  $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier)
+    {
+        $model = $this->createDynamicModel(\PesquisaProjeto\MinhaUfopUser::class);
+        
+        return $model->newQuery()
+            ->where($model->getAuthIdentifierName(), $identifier)
+            ->first();
+    }    
+}
