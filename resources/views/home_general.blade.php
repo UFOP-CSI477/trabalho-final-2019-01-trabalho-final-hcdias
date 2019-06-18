ef926ec946adfe49df97c416e3384b5a536f06b4
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-06-18 15:52:26 -0300

    Corrigir identacao

--- resources/views/home_general.blade.php
+++ resources/views/home_general.blade.php
@@ -77,22 +77,22 @@
       </div>
     </div>
     <div class="col-md-8">
-            <div class="box box-primary collapsed-box">
-              <div class="box-header">
-                <h3 class="box-title">Calendário de TCCs</h3>
-                <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+      <div class="box box-primary collapsed-box">
+        <div class="box-header">
+          <h3 class="box-title">Calendário de TCCs</h3>
+          <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
 
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-            </div>
-              </div>
-              <div class="box-body no-padding">
-                <div class="google-calendar">
-                  <iframe src="https://calendar.google.com/calendar/embed?src=548rvo9tv5peavk3gqc3gosd6o%40group.calendar.google.com&ctz=America%2FSao_Paulo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-                </div>
-              </div>
-            </div>
+          <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+      </div>
+        </div>
+        <div class="box-body no-padding">
+          <div class="google-calendar">
+            <iframe src="https://calendar.google.com/calendar/embed?src=548rvo9tv5peavk3gqc3gosd6o%40group.calendar.google.com&ctz=America%2FSao_Paulo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
           </div>
+        </div>
+      </div>
+    </div>
   </div>  
   <input type="hidden" value="{{$pesquisas}}" id="pesquisasTotal"/>
   <input type="hidden" value="{{$tccs}}" id="tccsTotal"/>
