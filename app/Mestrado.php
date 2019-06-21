cc9a794d773cfd4a55bf23b0b6e8327ae1271151
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-06-20 21:27:49 -0300

    Remover sufixo _mestrado da model mestrado

--- app/Mestrado.php
+++ app/Mestrado.php
@@ -26,43 +26,43 @@ class Mestrado extends Model
 		];
 
 	public function orientador(){
-		return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','orientador_mestrado_id');
+		return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','orientador_id');
 	}
 
 	public function coorientador(){
-		return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','coorientador_mestrado_id');
+		return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','coorientador_id');
 	}
 
     public function aluno(){
-    	return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','aluno_mestrado_id');
+    	return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','aluno_id');
     }
 
 	public function abordagem(){
-    	return $this->belongsTo('PesquisaProjeto\AbordagemPesquisa','abordagem_mestrado_id');
+    	return $this->belongsTo('PesquisaProjeto\AbordagemPesquisa','abordagem_id');
     }
 
 
     public function area(){
-    	return $this->belongsTo('PesquisaProjeto\AreaPesquisa','area_mestrado_id');
+    	return $this->belongsTo('PesquisaProjeto\AreaPesquisa','area_id');
     }
 
     public function natureza(){
-    	return $this->belongsTo('PesquisaProjeto\NaturezaPesquisa','natureza_mestrado_id');
+    	return $this->belongsTo('PesquisaProjeto\NaturezaPesquisa','natureza_id');
     }
 
     public function objetivo(){
-    	return $this->belongsTo('PesquisaProjeto\ObjetivoPesquisa','objetivo_mestrado_id');
+    	return $this->belongsTo('PesquisaProjeto\ObjetivoPesquisa','objetivo_id');
     }
 
     public function procedimento(){
-    	return $this->belongsTo('PesquisaProjeto\ProcedimentosPesquisa','procedimentos_mestrado_id');
+    	return $this->belongsTo('PesquisaProjeto\ProcedimentosPesquisa','procedimentos_id');
     }
 
     public function status(){
-    	return $this->belongsTo('PesquisaProjeto\StatusPesquisa','status_mestrado');
+    	return $this->belongsTo('PesquisaProjeto\StatusPesquisa','status_id');
     }
 
     public function subarea(){
-    	return $this->belongsTo('PesquisaProjeto\SubAreaPesquisa','sub_area_mestrado_id');
+    	return $this->belongsTo('PesquisaProjeto\SubAreaPesquisa','sub_area_id');
     }
 }
