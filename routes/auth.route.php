6ee0cf0ebbc3f6781a6fec2b414df14b69f89ce2
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-06-26 01:17:06 -0300

    Revert "Modularizar rotas"
    This reverts commit e89a30b190aba3e25df78a470be9e72055de44fb.

--- routes/auth.route.php
+++
@@ -1,9 +0,0 @@
-<?php
-Route::get('/login','Auth\MinhaUfopLoginController@showLoginUfop')->name('login');
-Route::post('/login','Auth\MinhaUfopLoginController@login')->name('minhaufop_login_submit');
-
-Route::post('/logout','Auth\MinhaUfopLoginController@logout')->name('minhaufop_logout_submit');
-
-Route::get('/admin/login','Auth\LoginController@showLoginForm')->name('admin_login');
-Route::post('/admin/login','Auth\LoginController@login')->name('admin_login_submit');
-Route::post('/admin/logout','Auth\LoginController@logout')->name('admin_logout');
