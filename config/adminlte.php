c39f8ff221e0d853e26638db43b66b4d0ee338a9
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-07-12 17:08:08 -0300

    Adicionar menu para proposta de TCC

--- config/adminlte.php
+++ config/adminlte.php
@@ -160,18 +160,38 @@ return [
                     'icon'  => 'plus',
                     'roles' => ['admin','aluno'],
                 ],
+            ]
+        ],
+        [
+            'text'      => 'Propostas de TCC',
+            'url'       => '#',
+            'icon'      => 'lightbulb-o',
+            'roles'     => ['admin','aluno','professor'],
+            'submenu'   => [
                 [
-                    'text'  => 'Proposta de TCC',
-                    'url'   => 'tcc/criar-proposta-tcc',
+                    'text'  =>  'Visualizar',
+                    'url'   => 'proposta-tcc/visualizar-proposta',
+                    'icon'  => 'search',
+                    'roles' => ['admin','aluno']
+                ],
+                [
+                    'text'  => 'Cadastrar proposta',
+                    'url'   => 'proposta-tcc/criar-proposta-tcc',
                     'icon'  => 'plus',
                     'roles' => ['admin','aluno'],
+                ],
+                [
+                    'text'  => 'Visualizar propostas',
+                    'url'   => 'proposta-tcc/visualizar-proposta-professor',
+                    'icon'  => 'search',
+                    'roles' => ['admin','professor'],
                 ]
             ]
         ],
         [
             'text'        => 'Mestrado',
             'url'         => '#',
-            'icon'        => 'book',
+            'icon'        => 'mortar-board',
             'roles'       => ['admin','professor','aluno'],
             'submenu'     => [
                 [
