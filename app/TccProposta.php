921e0a5b3b8729e06a1d8f6c819bfceb78571d1e
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-07-12 17:19:59 -0300

    Adicionar model TccProposta

---
+++ app/TccProposta.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PesquisaProjeto;
+
+use Illuminate\Database\Eloquent\Model;
+use PesquisaProjeto\Traits\TccPropostaEventsTrait;
+class TccProposta extends Model
+{
+
+    use TccPropostaEventsTrait;
+
+    protected $fillable = [
+        'orientador_id',
+        'coorientador_id',
+        'aluno_id',
+        'area_id',
+        'titulo', 
+        'descricao',
+        'status'
+    ];
+
+    public function orientador()
+    {
+        return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','orientador_id');
+    }
+
+    public function coorientador()
+    {
+        return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','coorientador_id');
+    }    
+
+    public function aluno(){
+        return $this->belongsTo('PesquisaProjeto\MinhaUfopUser','aluno_id');
+    } 
+
+    public function area(){
+        return $this->belongsTo('PesquisaProjeto\AreaPesquisa','area_id');
+    } 
+
+    public function status(){
+        return $this->belongsTo('PesquisaProjeto\StatusTccProposta','status_id');
+    }    
+}
